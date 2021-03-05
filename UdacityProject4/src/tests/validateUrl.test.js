2c3bc8b21d4d25b8fab89bc5d5cbe35a18324924
Naimah Muhammed-Yakubu <naimah.muhammed-yakubu@uwe.ac.uk>
2021-03-05 21:06:48 +0000

    Add files via upload

---
+++ UdacityProject4/src/tests/validateUrl.test.js
@@ -0,0 +1,7 @@
+import { TestScheduler } from 'jest'
+import { validateUrl } from '../client/js/validateUrl'
+
+test('validate url', () => {
+    expect(validateUrl('https://www.google.com')).toBe(true);
+    expect(validateUrl('httbs://www.google.con')).toBe(false);
+});
